f962dadf39529c704198cb909d8dfac4643bff92
Mrinmoy Sarkar <mrin2ksarkar@gmail.com>
2021-01-03 21:28:41 +0530

    Add files via upload

---
+++ os_code/cpu.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+#include <unistd.h>
+#include <assert.h>
+
+int main(int argc, char *argv[])
+{
+
+	if (argc != 2)
+	{
+		fprintf(stderr, "usage: cpu <string>\n");
+		exit(1);
+	}
+	char *str = argv[1];
+	while(1)
+	{
+		printf("%s\n", str);
+		sleep(1);
+	}
+	return 0;
+}
